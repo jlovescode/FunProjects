98722c7bf25ddd951e6c7ab23ba40e7f1cb771a2
Jeswin Godwin <jesgodwin@gmail.com>
2019-10-06 23:21:33 -0700

    added more comments

--- Algorithms/src/main/java/com/godwije/algos/FastSearch.java
+++ Algorithms/src/main/java/com/godwije/algos/FastSearch.java
@@ -3,6 +3,8 @@ package com.godwije.algos;
 import java.util.HashSet;
 import java.util.Set;
 
+//For just one search this will not be that faster but for multiple searches this is faster
+//It uses a set to store the strings for fast searching
 public class FastSearch {
     // fast Search is faster
     private Set<String> set;
